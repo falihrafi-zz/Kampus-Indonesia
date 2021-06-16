9fdeeeff41630fa510b2affd0b2af4d0e5e9129e
Rafi Falih Mulia <47825673+falihrafi@users.noreply.github.com>
2021-06-16 21:29:28 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Introduction></Introduction>
+        <Rekomendasi></Rekomendasi>
+        <LokasiKampus></LokasiKampus>
+        <Fitur></Fitur>
+        <Footer></Footer>
+    </div>
+</template>
+
+
+<script>
+import Header from './components/Header.vue'
+import Introduction from './components/Introduction.vue'
+import Rekomendasi from './components/Rekomendasi.vue'
+import LokasiKampus from './components/LokasiKampus.vue'
+import Fitur from './components/Fitur.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+    name: 'App',
+    components : {
+        Header,
+        Introduction,
+        Rekomendasi,
+        LokasiKampus,
+        Fitur,
+        Footer
+    }
+}
+</script>
+
+
+<style>
+
+</style>
