1e5b3b2feb31568e7a07ffa44d29e4bf79601f1e
Rafi Falih Mulia <47825673+falihrafi@users.noreply.github.com>
2021-06-16 21:31:19 +0700

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id = "header">
+        <img src = './resources/logokampusindonesia.jpg'>
+        <a>Beranda</a>
+        <a>Fitur</a>
+        <a>Hubungi Kami</a>
+        <a>Blog</a>
+        <a>Masuk</a>
+        <a>Daftar</a>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+
+    #header {
+        font-family : 'Quicksand', sans-serif;
+        display : flex;
+        width : 100%;
+    }
+
+    #header a {
+        display : inline-block;
+        text-align : center;
+        font-size : 120%;
+        margin-right : 1%;
+        margin-left : 1%;
+        margin-top : 2%;
+        margin-bottom : 1%;
+        font-weight : 300;
+        color : #696969;
+        cursor : pointer;
+    }
+
+    #header a:hover {
+        color : #000000;
+    }
+
+    #header img {
+        height : 14%;
+        width : 14%;
+        margin-left : 5%;
+    }
+</style>
